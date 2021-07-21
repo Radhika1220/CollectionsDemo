0d342f4e49759853f3d84f9e2772cffe24b87aa2
Radhika <radhika.shankar1220@gmail.com>
2021-07-21 18:36:08 +0530

    [Radhika] Initial Commit

---
+++ CollectionsDemo/List.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionsDemo
+{
+    /// <summary>
+    /// Collection-List Demo
+    /// </summary>
+    public class List
+    {
+        public static void ListDemo()
+
+        {
+            //Initializing the list
+            var cities = new List<string>();
+
+            //Array with 3 string elements
+            string[] popularCities = new string[3] { " San fransico", " Germany", "Manchester" };
+
+            //Addding elements to list
+            cities.Add("New York");
+            cities.Add("London");
+            cities.Add("Paris");
+            cities.Add("France");
+
+            //Accessing the list
+            Console.WriteLine("\nCities list are :\n");
+            foreach(var c in cities)
+            {
+                Console.WriteLine("The cities are : {0} ", c);
+            }
+
+            //AddRange-Adding range of elements(like array)
+            cities.AddRange(popularCities);
+            Console.WriteLine("\nAfter adding more cities list are :\n ");
+            foreach (var c in cities)
+            {
+                Console.WriteLine("The cities are : {0} ", c);
+            }
+
+        }
+    }
+}
